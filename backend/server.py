4f8002dc4a3faf020e9388ff59ced6bab25db1c5
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-07-13 23:09:15 +0530

    backend part deploying

--- backend/server.py
+++ backend/server.py
@@ -7,9 +7,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__,static_url_path="/static")
 mysql = MySQL(app)
 CORS(app)
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '$uryA11472'
-app.config['MYSQL_DB'] = 'twitterclone'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-02.cleardb.com'
+app.config['MYSQL_USER'] = 'b3aae55346c19a'
+app.config['MYSQL_PASSWORD'] = 'a5f740e4'
+app.config['MYSQL_DB'] = 'heroku_204498ff42240e5'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 @app.route("/")
